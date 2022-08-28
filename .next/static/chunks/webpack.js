2b415ce528740f3a6d2927f95e504d0ee0b323da
Josssiiiah <102438546+Josssiiiah@users.noreply.github.com>
2022-08-28 15:27:25 -0500

    Bug Fixing

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bdc77faae94e4594"; }
+/******/ 		__webpack_require__.h = function() { return "139632a10b8b7851"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+//final
 
 function MyApp({ Component, pageProps }) {
   return (
